83c87796bf09f4223688d17220a6f62a38a493f8
MachinicGlitch <annalise.alexandra64@gmail.com>
2023-11-18 22:43:07 -0800

    expanding character sheet and proptype validation

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -9,20 +9,20 @@ import Skills from './Skills'
 import Stats from './Stats'
 
 function Character({ characterData }) {
-    const { character_name, ability_scores, equipment, proficiencies } = characterData.character
+    const { description, ability_scores, status, saves, equipment, proficiencies, skill_ranks } = characterData.character
     const { weapons } = equipment
 
     return (
         <>
             <div>
                 <h2>{`${characterData.player_name}'s Character: `}</h2>
-                <Biography character_name={character_name} />
+                <Biography description={description} />
                 <Ability_Scores ability_scores={ability_scores} />
-                <Stats />
-                <Saves />
+                <Stats status={status} />
+                <Saves saves={saves} />
                 <Attacks weapons={weapons} />
                 <Feats />
-                <Skills />
+                <Skills skill_ranks={skill_ranks} />
             </div>
         </>
     )
@@ -37,24 +37,51 @@ const subSkillShape = PropTypes.shape({
     ranks: PropTypes.string.isRequired
 })
 
-//Defining shape for skills
-const skillRanksShape = Proptypes.shape({
-    Appraise: PropTypes.number,
-    Balance: PropTypes.number,
-    Bluff: PropTypes.number,
-    Climb: PropTypes.number,
-    Concentration: PropTypes.number,
-    Craft: PropTypes.arrayOf(subSkillShape),
-    DecipherScript: PropTypes.number,
-  // ... other skills
+// Defining shape for skills
+const skillRanksShape = PropTypes.shape({
+    appraise: PropTypes.number.isRequired,
+    balance: PropTypes.number.isRequired,
+    bluff: PropTypes.number.isRequired,
+    climb: PropTypes.number.isRequired,
+    concentration: PropTypes.number.isRequired,
+    craft: PropTypes.arrayOf(subSkillShape).isRequired,
+    'decipher script': PropTypes.number.isRequired,
+    diplomacy: PropTypes.number.isRequired,
+    'disable device': PropTypes.number.isRequired,
+    disguise: PropTypes.number.isRequired,
+    'escape artist': PropTypes.number.isRequired,
+    forgery: PropTypes.number.isRequired,
+    'gather information': PropTypes.number.isRequired,
+    'handle animal': PropTypes.number.isRequired,
+    heal: PropTypes.number.isRequired,
+    intimidate: PropTypes.number.isRequired,
+    jump: PropTypes.number.isRequired,
+    knowledge: PropTypes.arrayOf(subSkillShape).isRequired,
+    listen: PropTypes.number.isRequired,
+    'move silently': PropTypes.number.isRequired,
+    'open lock': PropTypes.number.isRequired,
+    perform: PropTypes.arrayOf(subSkillShape).isRequired,
+    profession: PropTypes.arrayOf(subSkillShape).isRequired,
+    ride: PropTypes.number.isRequired,
+    search: PropTypes.number.isRequired,
+    'sense motive': PropTypes.number.isRequired,
+    'sleight of hand': PropTypes.number.isRequired,
+    'speak language': PropTypes.number.isRequired,
+    spellcraft: PropTypes.number.isRequired,
+    swim: PropTypes.number.isRequired,
+    tumble: PropTypes.number.isRequired,
+    'use magic device': PropTypes.number.isRequired,
+    'use rope': PropTypes.number.isRequired
 })
 
-
+// Defining shape of imported character sheet
 Character.propTypes = {
     characterData: PropTypes.shape({
         player_name: PropTypes.string.isRequired,
         character: PropTypes.shape({
-            character_name: PropTypes.string.isRequired,
+            description: PropTypes.shape({
+                character_name: PropTypes.string.isRequired,
+            }).isRequired,
             ability_scores: PropTypes.shape({
                 str: PropTypes.number.isRequired,
                 dex: PropTypes.number.isRequired,
@@ -62,6 +89,16 @@ Character.propTypes = {
                 int: PropTypes.number.isRequired,
                 wis: PropTypes.number.isRequired,
                 cha: PropTypes.number.isRequired,
+            }).isRequired,
+            status: PropTypes.shape({
+                'max hp': PropTypes.number.isRequired,
+                'current hp': PropTypes.number.isRequired,
+                'temp hp': PropTypes.number.isRequired
+            }),
+            saves: PropTypes.shape({
+                reflex: PropTypes.number.isRequired,
+                fortitude: PropTypes.number.isRequired,
+                will: PropTypes.number.isRequired
             }).isRequired,
             equipment: PropTypes.shape({
                 weapons: PropTypes.arrayOf(
@@ -112,7 +149,7 @@ Character.propTypes = {
                 tools: PropTypes.arrayOf(PropTypes.string).isRequired,
                 languages: PropTypes.arrayOf(PropTypes.string).isRequired
             }).isRequired,
-            skill_ranks: skillRanksShape.isRequired
+            skill_ranks: skillRanksShape.isRequired,
         }).isRequired,
     }).isRequired,
 }
